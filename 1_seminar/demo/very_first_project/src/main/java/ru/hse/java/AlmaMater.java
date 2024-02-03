682f275a71f30f046bbff3e1a3afcee93a13ccf7
bromivipo <misha.pototsky@gmail.com>
2024-02-03 18:09:32 +0300

    hw done

--- 1_seminar/demo/very_first_project/src/main/java/ru/hse/java/AlmaMater.java
+++ 1_seminar/demo/very_first_project/src/main/java/ru/hse/java/AlmaMater.java
@@ -1,12 +1,79 @@
 package ru.hse.java;
 
-import java.time.ZonedDateTime;
+import java.util.Objects;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * Реализовать методы
- * */
-public interface AlmaMater {
+ */
+public class AlmaMater {
 
-    ZonedDateTime getDateOfGraduation();
+    //    ZonedDateTime getDateOfGraduation(); // этот метод перенес в класс студента
 
+    private static final String[] name_array = new String[]{"Гимназия", "Школа", "Лицей"};
+    private static final Pattern CITY_MASK = Pattern.compile("[А-Яа-я ]+");
+
+    static boolean nameIsValid(String name) {
+        return Arrays.asList(name_array).contains(name);
+    }
+
+    static boolean cityIsValid(String city) {
+        return CITY_MASK.matcher(city).matches();
+    }
+
+    public AlmaMater(String city, String name, Integer number) {
+        Objects.requireNonNull(city);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(number);
+        if (!nameIsValid(name)) {
+            throw new IllegalArgumentException(name + "is not acceptable. Only \"Гимназия\", \"Школа\", \"Лицей\" are allowed");
+        }
+        if (!cityIsValid(name)) {
+            throw new IllegalArgumentException( city + " has not valid by " + CITY_MASK + " pattern.");
+        }
+        this.name = name;
+        this.city = city;
+        this.number = number;
+    }
+
+    public String getFullName() {
+        return name +  " №" + number.toString() + " г. " + city;
+    }
+
+    public String toString() {
+        return getFullName();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        AlmaMater alma = (AlmaMater) obj;
+
+        return Objects.equals(name, alma.name) &&
+                Objects.equals(city, alma.city) &&
+                Objects.equals(number, alma.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, city, number);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    private final String name;
+    private final String city;
+    private final Integer number;
 }
--- 1_seminar/demo/very_first_project/src/test/java/ru/hse/java/SchoolTest.java
+++ 1_seminar/demo/very_first_project/src/test/java/ru/hse/java/SchoolTest.java
@@ -3,6 +3,15 @@ package ru.hse.java;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 /**
  * Test, which checks student group has all almamaters with graduation date
  * */
@@ -20,4 +29,52 @@ public class SchoolTest {
         Assertions.assertFalse(School.nameIsValid(name));
     }
 
+    @Test
+    public void testStudentsHaveSameAlma() {
+        AlmaMater gymn = new AlmaMater("Москва", "Гимназия", 1);
+        final Set<Student> EXAMPLE = new HashSet<>(Set.of(
+                new Student("Катя", gymn, ZonedDateTime.now()),
+                new Student("Вася", gymn, ZonedDateTime.now()),
+                new Student("Петя", gymn, ZonedDateTime.now()),
+                new Student("Настя", gymn, ZonedDateTime.now())));
+        for (var stud : EXAMPLE) {
+            Assertions.assertEquals(stud.getAlmaMater(), gymn);
+        }
+    }
+
+    @Test
+    public void testStudentsHaveDifferentAlmas() {
+        AlmaMater gymn = new AlmaMater("Москва", "Гимназия", 1);
+        AlmaMater lyceum = new AlmaMater("Москва", "Лицей", 1);
+        final Set<Student> EXAMPLE = new HashSet<>(Set.of(
+                new Student("Катя", gymn, ZonedDateTime.now()),
+                new Student("Вася", gymn, ZonedDateTime.now()),
+                new Student("Петя", lyceum, ZonedDateTime.now()),
+                new Student("Настя", lyceum, ZonedDateTime.now())));
+        Assertions.assertFalse(EXAMPLE.stream().allMatch((s) -> s.getAlmaMater().equals(gymn)));
+    }
+
+    @Test
+    public void testStudentsHaveSameGradYear() {
+        AlmaMater gymn = new AlmaMater("Москва", "Гимназия", 1);
+        AlmaMater lyceum = new AlmaMater("Москва", "Лицей", 1);
+        final Set<Student> EXAMPLE = new HashSet<>(Set.of(
+                new Student("Катя", gymn, LocalDateTime.parse("2019-05-31T12:15:30").atZone(ZoneId.of("Europe/Moscow"))),
+                new Student("Вася", gymn, LocalDateTime.parse("2019-05-31T12:15:30").atZone(ZoneId.of("Europe/Moscow"))),
+                new Student("Петя", lyceum, LocalDateTime.parse("2019-05-31T12:15:30").atZone(ZoneId.of("Europe/Moscow"))),
+                new Student("Настя", lyceum, LocalDateTime.parse("2019-05-31T12:15:30").atZone(ZoneId.of("Europe/Moscow")))));
+        Assertions.assertTrue(EXAMPLE.stream().allMatch((s) -> s.getGraduation().getYear() == 2019));
+    }
+
+    @Test
+    public void testStudentsHaveDifferentGradYear() {
+        AlmaMater gymn = new AlmaMater("Москва", "Гимназия", 1);
+        AlmaMater lyceum = new AlmaMater("Москва", "Лицей", 1);
+        final Set<Student> EXAMPLE = new HashSet<>(Set.of(
+                new Student("Катя", gymn, LocalDateTime.parse("2019-05-31T12:15:30").atZone(ZoneId.of("Europe/Moscow"))),
+                new Student("Вася", gymn, LocalDateTime.parse("2020-05-31T12:15:30").atZone(ZoneId.of("Europe/Moscow"))),
+                new Student("Петя", lyceum, LocalDateTime.parse("2021-05-31T12:15:30").atZone(ZoneId.of("Europe/Moscow"))),
+                new Student("Настя", lyceum, LocalDateTime.parse("2022-05-31T12:15:30").atZone(ZoneId.of("Europe/Moscow")))));
+        Assertions.assertFalse(EXAMPLE.stream().allMatch((s) -> s.getGraduation().getYear() == 2019));
+    }
 }
